480102378e7440ca1001d63ad8b3c9a80423524f
bmichaelsen <bmichaelsen@d3b9dcdd-531f-0410-bf3a-d49ed6d32da3>
2007-04-30 11:54:43 +0000

    cleanup in dist/

--- trunk/install.sh
+++ trunk/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python setup.py sdist
 cp dist/*.tar.gz /usr/portage/distfiles
-cp dist/*1.2.ebuild /usr/local/portage/app-portage/etcproposals
+cp dist/*.ebuild /usr/local/portage/app-portage/etcproposals
 cd /usr/local/portage/app-portage/etcproposals
 ebuild *.ebuild digest
+emerge etcproposals
