23d56868f4ce6e5c29953b443fb867c6f34dd9db
bmichaelsen <bmichaelsen@d3b9dcdd-531f-0410-bf3a-d49ed6d32da3>
2007-05-01 12:14:01 +0000

    completed install script

--- trunk/install.sh
+++ trunk/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python setup.py sdist
+mkdir -p /usr/local/portage/app-portage/etcproposals
 cp dist/*.tar.gz /usr/portage/distfiles
 cp dist/*.ebuild /usr/local/portage/app-portage/etcproposals
 cd /usr/local/portage/app-portage/etcproposals
