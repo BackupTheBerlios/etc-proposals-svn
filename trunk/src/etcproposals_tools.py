e0434ea2c528b34f43b4022fdd6b3ea8ba2c2dd2
bmichaelsen <bmichaelsen@d3b9dcdd-531f-0410-bf3a-d49ed6d32da3>
2007-05-01 15:09:52 +0000

    further work on PyQt EINTR workaround

--- trunk/src/etcproposals_tools.py
+++ trunk/src/etcproposals_tools.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 #! -*- coding: utf-8 -*-
 # Copyright 2006, 2007 Björn Michaelsen
-# based on gentoo portage 2.1.1, Copyright 1998-2007 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 import subprocess, errno
 
 # helper needed because PyQt seems to cause interrupted system calls
 def get_command_output_iterator(command_and_args):
-    outpipe = subprocess.Popen(command_and_args, shell=False, stdout=subprocess.PIPE).stdout
+    process = None
     eof = False
+    while process == None:
+        try:
+            process = subprocess.Popen(command_and_args, shell=False, stdout=subprocess.PIPE)
+        except (OSError, IOError), e:
+            if e.errno != errno.EINTR:
+                raise e
     while not eof:
         try:
-            for line in outpipe.readlines():
+            for line in process.stdout.readlines():
                 yield line
             eof = True
         except (OSError, IOError), e:
