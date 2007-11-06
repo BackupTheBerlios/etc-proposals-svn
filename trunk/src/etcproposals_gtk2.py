715b02a5e475ff9a1729e4774e42b5f68e6fc474
bmichaelsen <bmichaelsen@d3b9dcdd-531f-0410-bf3a-d49ed6d32da3>
2007-11-06 23:19:48 +0000

    gtk-frontend: fixed icons for inverted gtk-themes

--- trunk/src/etcproposals_gtk2.py
+++ trunk/src/etcproposals_gtk2.py
@@ -602,7 +602,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA''')
-        self.set_authors(['Björn Michaelsen', 'Jeremy Wickersheimer', 'Christian Glindkamp', 'Jakub Steiner'])
+        self.set_authors(['Björn Michaelsen', 'Jeremy Wickersheimer', 'Christian Glindkamp', 'Jakub Steiner', 'Andreas Nilsson'])
         self.show_all()
         self.connect("response", lambda *d: self.destroy())
 
